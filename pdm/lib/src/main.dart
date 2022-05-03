3d670b71a32f53e757b111f1809056c6f7238f8f
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-05-03 19:59:57 -0300

    Update main.dart

--- pdm/lib/src/main.dart
+++ pdm/lib/src/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Pingu Idiomas',
+        title: 'Pingu Slang',
         home: SplashScreen()); // LoginScreen());
   }
 }
